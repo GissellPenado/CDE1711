654f5682954c812e8b59b5b0a7a44488fe094edc
GissellPenado <gpenadom@uamv.edu.ni>
2025-11-16 21:31:44 -0600

    Agregué la clase "Nodo"

---
+++ CDE1711/Nodo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDE1711
+{
+    public class Nodo
+    {
+        public string Nombre { get; set; }
+        public List<Nodo> Hijos { get; set; }
+
+        public Nodo (string nomnbre)
+        {
+            Nombre = Nombre;
+            Hijos = new List<Nodo>();
+        }
+    }
+}

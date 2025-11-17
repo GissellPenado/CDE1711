5b3ad7131383353a45854695cd0c0df0a325c916
GissellPenado <gpenadom@uamv.edu.ni>
2025-11-16 22:47:32 -0600

    Agregué la clase "Grafo", y, dentro de esta, un diccionario que implementa la lista de adyacencia, el constructor del grafo, y las funciones AgregarNodo, AgregarArista y MostrarConexiones

---
+++ CDE1711/Grafo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDE1711
+{
+    public class Grafo
+    {
+        private Dictionary<string, List<(string destino, int peso)>> ady;
+
+        public Grafo()
+        {
+            ady = new Dictionary<string, List<(string, int)>>();
+        }
+
+        public void AgregarNodo(string nombre)
+        {
+            if (!ady.ContainsKey(nombre))
+                ady[nombre] = new List<(string, int)>();
+        }
+
+        public void AgregarArista(string a, string b,int peso)
+        {
+            AgregarNodo(a);
+            AgregarNodo(b);
+
+            ady[a].Add((b, peso));
+            ady[b].Add((a, peso));
+
+        }
+
+        public void MostrarConexiones()
+        {
+            foreach (var nodo in ady)
+            {
+                Console.WriteLine($"{nodo.Key}:");
+                foreach (var (dest, peso) in nodo.Value)
+                    Console.WriteLine($"  -> {dest} (peso: {peso})");
+            }
+        }
+
+    }
+}
